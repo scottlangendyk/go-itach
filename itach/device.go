131d7aff2b4944eb4a0e8a72a647ff5c406b0134
Scott Langendyk <scott@langendyk.com>
2015-08-31 18:14:04 -0400

    Move sensor notify functions to more appropriate ip2ir.go file

--- itach/device.go
+++ itach/device.go
@@ -21,25 +21,3 @@ func (d *Device) Connect() (*DeviceConn, error) {
 
   return &DeviceConn{conn}, nil
 }
-
-// ListenToSensors listens for sensor notification
-// from an IP2IR device on the default port.
-func (d *Device) ListenToSensors() (l *SensorListener, err error) {
-  return d.ListenToSensorsOnPort(9132)
-}
-
-// ListenToSensors listens for sensor notifications
-// from an IP2IR device on the specified port.
-func (d *Device) ListenToSensorsOnPort(port int) (l *SensorListener, err error) {
-  addr, err := net.ResolveUDPAddr("udp4", fmt.Sprintf("239.255.250.250:%d", port))
-  if err != nil {
-    return nil, err
-  }
-
-  conn, err := net.ListenMulticastUDP("udp4", nil, addr)
-  if err != nil {
-    return nil, err
-  }
-
-  return &SensorListener{conn: &DeviceConn{conn}}, nil
-}
--- itach/ip2ir.go
+++ itach/ip2ir.go
@@ -122,3 +122,25 @@ func (conn *DeviceConn) SendIR(connectorAddress int, command *IRCommand) error {
 
   return nil
 }
+
+// ListenToSensors listens for sensor notification
+// from an IP2IR device on the default port.
+func (d *Device) ListenToSensors() (l *SensorListener, err error) {
+  return d.ListenToSensorsOnPort(9132)
+}
+
+// ListenToSensors listens for sensor notifications
+// from an IP2IR device on the specified port.
+func (d *Device) ListenToSensorsOnPort(port int) (l *SensorListener, err error) {
+  addr, err := net.ResolveUDPAddr("udp4", fmt.Sprintf("239.255.250.250:%d", port))
+  if err != nil {
+    return nil, err
+  }
+
+  conn, err := net.ListenMulticastUDP("udp4", nil, addr)
+  if err != nil {
+    return nil, err
+  }
+
+  return &SensorListener{conn: &DeviceConn{conn}}, nil
+}
